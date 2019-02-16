485bd2759e7430e936093cf84d6d2505298b8fda
Oleh Gerus <gerus.oleg@gmail.com>
2019-02-16 14:09:03 +0200

    configured sass and bootstrap

--- SocialCareProject/App_Start/BundleConfig.cs
+++ SocialCareProject/App_Start/BundleConfig.cs
@@ -24,12 +24,17 @@ namespace SocialCareProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/general_styles.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                 "~/Scripts/knockout-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/common").Include(
                 "~/Scripts/common.js"));
+
+            //sass
+            bundles.Add(new StyleBundle("~/Content/sassCustom").Include(
+                "~/Content/custom/customer_styles.css"));
+
         }
     }
 }
