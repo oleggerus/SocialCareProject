fa7e216699e01b005a265a514d6737ed55b8ffde
Oleh Gerus <gerus.oleg@gmail.com>
2019-06-01 16:04:57 +0300

    styles

--- SocialCareProject/Areas/Customer/Controllers/HomeController.cs
+++ SocialCareProject/Areas/Customer/Controllers/HomeController.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataRepository.Extensions;
 using Services;
+using Services.People;
+using SocialCareProject.Areas.Customer.Models.Home;
+using SocialCareProject.Authentication;
 
 namespace SocialCareProject.Areas.Customer.Controllers
 {
     public class HomeController : BaseCustomerController
     {
         private readonly IAuthenticationService _authenticationService;
+        private readonly ICustomerService _customerService;
+
         public HomeController(
-            IAuthenticationService authenticationService)
+            IAuthenticationService authenticationService,
+            ICustomerService customerService)
         {
-           
+            _customerService = customerService;
             _authenticationService = authenticationService;
         }
         // GET: Customer/Home
         public ActionResult Index()
         {
-            return View();
+            var currentUser = HttpContext.User as CustomUser;
+            var id = currentUser?.UserId ?? default(int);
+
+            var customer = _customerService.GetCustomerByUserId(currentUser.UserId);
+            if (customer == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
+            var model = new HomeCustomerModel
+            {
+                Id = id,
+                Name = customer.User.GetFullName()
+            };
+
+            return View(model);
         }
        
     }
---
+++ SocialCareProject/Areas/Customer/Models/Home/HomeCustomerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialCareProject.Areas.Customer.Models.Home
+{
+    public class HomeCustomerModel
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+    }
+}
