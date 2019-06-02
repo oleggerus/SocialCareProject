d9344ecd39676b5cbb669e02710a0321e17380d7
oleg.gerus <gerus.oleg@gmail.com>
2019-06-02 14:37:25 +0300

    comments

--- Services/Assignments/WorkerPersonAssignmentService.cs
+++ Services/Assignments/WorkerPersonAssignmentService.cs
@@ -8,13 +8,30 @@ using DataRepository.Enums;
 
 namespace Services.Assignments
 {
-    
+    /// <summary>
+    /// Class implemented IWorkerPersonAssignmentService interface
+    /// </summary>
     public class WorkerPersonAssignmentService : IWorkerPersonAssignmentService
     {
-
+        /// <summary>
+        /// The assignment repository
+        /// </summary>
         private readonly IRepository<WorkerPersonAssignment> _assignmentRepository;
+
+        /// <summary>
+        /// The worker repository
+        /// </summary>
         private readonly IRepository<Worker> _workerRepository;
 
+        /// <summary>
+        /// The maximum amount of active assignments for social worker
+        /// </summary>
+        private const int MaxAmountOfActiveAssignments = 6;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerPersonAssignmentService"/> class.
+        /// </summary>
+        /// <param name="assignmentRepository">The assignment repository.</param>
+        /// <param name="workerRepository">The worker repository.</param>
         public WorkerPersonAssignmentService(
             IRepository<WorkerPersonAssignment> assignmentRepository,
             IRepository<Worker> workerRepository)
@@ -23,17 +40,28 @@ namespace Services.Assignments
             _assignmentRepository = assignmentRepository;
         }
 
+        /// <summary>
+        /// Gets the allowed for assign workers.
+        /// </summary>
+        /// <param name="administrationId">The administration identifier.</param>
+        /// <returns></returns>
         public List<Worker> GetAllowedForAssignWorkers(int administrationId)
         {
             var excludedWorkers = _assignmentRepository.TableNoTracking
                 .Where(x => x.Worker.Administration.Id == administrationId)
                 .GroupBy(x => x.Worker)
-                .Where(x => x.Count() > 6)
+                .Where(x => x.Count() > MaxAmountOfActiveAssignments)
                 .Select(x => x.Key.Id).ToList();
 
-            return _workerRepository.TableNoTracking.Where(x =>x.Administration.Id == administrationId && !excludedWorkers.Contains(x.Id)).ToList();
+            return _workerRepository.TableNoTracking
+                .Where(x => x.Administration.Id == administrationId && !excludedWorkers.Contains(x.Id)).ToList();
         }
 
+        /// <summary>
+        /// Creates the specified assignment.
+        /// </summary>
+        /// <param name="assignment">The assignment.</param>
+        /// <returns></returns>
         public WorkerPersonAssignment Create(WorkerPersonAssignment assignment)
         {
             if (assignment == null)
@@ -45,12 +73,19 @@ namespace Services.Assignments
             return assignment;
         }
 
+        /// <summary>
+        /// Determines whether is worker free by specified worker user identifier.
+        /// </summary>
+        /// <param name="workerUserId">The worker user identifier.</param>
+        /// <returns>
+        /// true if worker is free; otherwise, false.
+        /// </returns>
         public bool IsWorkerFree(int workerUserId)
         {
             return _assignmentRepository.TableNoTracking.Count(x => x.Worker.UserId == workerUserId &&
                                                                     x.AssignmentStatusId ==
                                                                     (int) WorkerPersonAssignmentStatuses
-                                                                        .Активно) < 6;
+                                                                        .Активно) < MaxAmountOfActiveAssignments;
 
 
         }
